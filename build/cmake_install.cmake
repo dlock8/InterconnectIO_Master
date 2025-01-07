c7a1015860a630a696c14cc4045e40f831323222
dlock8 <daniel.lockhead@videotron.ca>
2025-01-07 11:13:12 -0500

    github compilation error fix #4

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -44,7 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/pi/pico/InterconnectIO_Master/build/firmware/cmake_install.cmake")
+  include("/home/pi/pico/InterconnectIO_Master/build/firmware/src/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
